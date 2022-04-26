f404c53ec247e93a21e94af50e0dc4b00fe7e4ca
Johanna Maier <joh.maier1@googlemail.com>
2022-04-27 00:21:16 +0200

    Added navbar template, added User fields to Devise templates, added Offer new vew.

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -7,8 +7,23 @@ class OffersController < ApplicationController
   end
 
   def new
+    @offer = Offer.new
   end
 
   def create
+    @offer = Offer.new(offer_params)
+    @user = current_user
+    @offer.user = @user
+    if @offer.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:title, :description, :price, :number_of_people, :category)
   end
 end
