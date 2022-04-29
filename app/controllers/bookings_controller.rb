1caee6ba59af1603efcaf003973e118ad89ca6fd
Anjali Kumar <anjali.kumar06@gmail.com>
2022-04-29 12:59:04 +0200

    Added Bookings controller and book an offer page

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,34 @@
+class BookingsController < ApplicationController
+  before_action :get_offer
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.offer = @offer
+    if @booking.save
+      redirect_to root_path # TODO: change this link
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def get_offer
+    @offer = Offer.find(params[:offer_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:comment, :address, :status, :booking_date, :start_time, :end_time)
+  end
+
+  # t.text "comment"
+  #   t.string "address"
+  #   t.string "status"
+  #   t.date "booking_date"
+  #   t.time "start_time"
+  #   t.time "end_time"
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :offers, only: [:index, :show, :new, :create]
+  resources :offers, only: [:index, :show, :new, :create] do
+    resources :bookings, only: [:new, :create]
+  end
 end
