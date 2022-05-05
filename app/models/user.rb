35ea28bd94683695c71c9cad5f9321564a2d39ee
Alexandra Stroe <alexandra.maria.stroe@gmail.com>
2022-05-05 21:11:24 +0200

    Added avatar photo for user registation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :offers, dependent: :destroy
   has_many :bookings
+  has_one_attached :avatar_photo
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -9,5 +10,6 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :avatar_photo, presence: true
   # :description can be optional
 end
